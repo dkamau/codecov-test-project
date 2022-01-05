c212d73e50158dd1920f48934ae81db69ce46e59
Dennis Kamau <34334743+dkamau@users.noreply.github.com>
2022-01-05 23:06:15 +0300

    Full test coverage

--- CodeCovSampleTestProject.UnitTests/TestClass_UnitTests.cs
+++ CodeCovSampleTestProject.UnitTests/TestClass_UnitTests.cs
@@ -3,19 +3,32 @@ using Xunit;
 
 namespace CodeCovSampleTestProject.UnitTests
 {
-    public class TestClass_UnitTests
+  public class TestClass_UnitTests
+  {
+    [Fact]
+    public void CanVote_RetunrsTrue_IfOver18()
     {
-        [Fact]
-        public void CanVote_RetunrsFalse_IfUnder18()
-        {
-            // Arrange
-            MyTestClass myTestClass = new MyTestClass();
+      // Arrange
+      MyTestClass myTestClass = new MyTestClass();
 
-            // Act 
-            var result = myTestClass.CanVote(DateTime.Now.AddYears(-17));
+      // Act 
+      var result = myTestClass.CanVote(DateTime.Now.AddYears(-17));
 
-            // Assert
-            Assert.False(result);
-        }
+      // Assert
+      Assert.False(result);
     }
+
+    [Fact]
+    public void CanVote_RetunrsFalse_IfUnder18()
+    {
+      // Arrange
+      MyTestClass myTestClass = new MyTestClass();
+
+      // Act 
+      var result = myTestClass.CanVote(DateTime.Now.AddYears(-17));
+
+      // Assert
+      Assert.False(result);
+    }
+  }
 }
